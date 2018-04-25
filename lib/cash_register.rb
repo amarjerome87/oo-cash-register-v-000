d95977f82c9ec5646b11460ce5e986a004abdc52
Amar Chatman <amarjerome@gmail.com>
2018-04-25 15:54:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require 'pry'
+class CashRegister
 
+attr_accessor :total, :discount, :title, :price, :quantity
+
+def initialize (discount = nil)
+  @total = 0
+  @discount = discount
+end
+
+def add_item (title,price, quantity = nil)
+  @title = title
+  @price = price
+  @quantity = quantity
+  title_array = []
+  title_array << @title
+  self.total += @price 
+end
+
+end
