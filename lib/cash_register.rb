e6e27c4c82b10abf1a997385beedd78463ded2eb
Amar Chatman <amarjerome@gmail.com>
2018-04-25 16:52:40 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ def add_item (title,price, quantity = 1)
 end
 
 def apply_discount
-  @total.percent_of(@discount)
+  @total.percent_of(@discount.to_i)
   puts "After the discount, the total comes to $800."
 end
 
